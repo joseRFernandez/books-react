18fa0b087e2373ecbd03e5f7807563f6dff37591
Jose_Fernandez <jfernand84@gmail.com>
2023-05-05 17:45:11 -0400

    createBook connected to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 const App = () => {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+
+    setBooks(() => response.data);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
 
     setBooks(() => updatedBooks);
   };
@@ -32,7 +47,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList books={books} onDelete={deleteBookById} onConfirmEdit={editBookById}/>
+      <BookList books={books} onDelete={deleteBookById} onConfirmEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
